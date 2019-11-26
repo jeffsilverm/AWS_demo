f00e9bd048c862cf19f1da0493f26c39c4c6002b
Jeffrey Silverman <jsilverman@juniper.net>
2019-11-25 19:25:06 -0800

    Passes all tests for backend.DICT type.

--- kv_pair2.py
+++ kv_pair2.py
@@ -16,8 +16,14 @@
 # OPTION - change the configuration to use different back ends
 # TRACE - report the configuration
 
+import importlib
+import json
+import os
+import random
+import string
+import uuid
 from enum import Enum, unique, auto
-from json import load
+import sys
 
 
 @unique
@@ -28,6 +34,7 @@ class Backends(Enum):
     SHELVES = auto()
     TEXTFILE = auto()
     MONGODB = auto()
+    DICT = auto()
 
 
 class Platforms(Enum):
@@ -38,29 +45,253 @@ class Platforms(Enum):
 
 class Configuration(object):
 
-    def __init__(self, filename: str):
+    def __init__(self, configuration_filename: str):
         """
-        :param filename: str: The name of the file that holds the
-        conifugration or None if the file should be created
+        :param configuration_filename: str The name of the file that holds the
+        configuration or None if the file should be created
+        :return None
         """
-        self.filename = filename
-        self.configuration['backend'] = Backends.MONGODB
-
-    def load_configuration(self, filename: str):
-        if filename is not None:
-            self.filename = filename
-        if self.filename is not None:
-            self.configuration = load(self.filename)
+        self.filename = configuration_filename
+        if configuration_filename is None:
+            # This is the file we're going to write to on exit.
+            self.filename = os.path.join(os.curdir,
+                                         "kv_pair2_configuration.json")
+            self.backend: Backends = Backends.MONGODB
         else:
-            # Load the configuration with default values
-            self.configuration = {"filename": "kv_pair2_configuration.json",
-                                  "backend": "local"}
+            configuration_dict = json.load(self.filename)
+            self.backend = configuration_dict['backend_']
 
 
+def random_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    """Generate a random string of size characters long
+    :size   int:    The length of the random string to generate
+    :chars  str:    A string of characters which is the set of characters from
+                    the random characters will be drawn
+    """
 
+    # To see the list of inspections, goto
+    # https://gist.github.com/pylover/7870c235867cf22817ac5b096defb768
+    # noinspection PyUnusedLocal
+    return ''.join(random.choice(chars) for x in range(size))
 
 
 class Backend(object):
-    """This class calls all of the backends """
+    """This class handles all of the interfacing to all of the
+    backends.  Thus, it is an implementation of an implementation
+     agnostic Key/Value server"""
 
-    pass
+    def __init__(self, backend_: Backends = Backends.SQLLIGHT):
+        """
+
+        :type backend_: Backends Which backend to use
+        """
+        self.backend = backend_
+        if self.backend == Backends.DICT:
+            db_con = dict()
+        elif self.backend == Backends.SQLLIGHT:
+            default_path = os.path.join(os.path.dirname(__file__),
+                                        'database.sqlite3')
+            db_con = self.initialize_sqllite(db_path=default_path)
+
+        elif self.backend == Backends.MONGODB:
+            db_con = self.initialize_mongodb()
+        else:
+            raise NotImplemented
+
+        self.db_con = db_con
+        self.gold = dict()
+
+    # CRUD - Create, Read, Update, Delete
+    def create(self, ckey, cvalue):
+        """
+        :param ckey: str: the key to search for
+        :param cvalue: str: the value to find
+        :return: None
+        Note: if trying to create a key/value pair on a key that already
+        exists, then just do it
+        """
+        if self.backend == Backends.DICT:
+            self.db_con[ckey] = cvalue
+        elif self.backend == Backends.DYNAMO_DB:
+            raise NotImplemented("create Backends.DYNAMO_DB")
+        elif self.backend == Backends.MONGODB:
+            raise NotImplemented("create Backends.MONGODB")
+        elif self.backend == Backends.MYSQL:
+            raise NotImplemented("create Backends.MYSQL")
+        elif self.backend == Backends.SHELVES:
+            raise NotImplemented("create Backends.SHELVES")
+        elif self.backend == Backends.SQLLIGHT:
+            raise NotImplemented("create Backends.SQLIGHT")
+        elif self.backend == Backends.TEXTFILE:
+            raise NotImplemented("create Backends.TEXTFILE")
+        else:
+            raise ValueError(
+                f"Backend type {self.backend} is not implemented or just wrong")
+
+    def read(self, r_key):
+        """
+
+        :param r_key: str: the key to search for
+        :return: str: the value that the key sought
+        """
+
+        if self.backend == Backends.DICT:
+            return self.db_con[r_key]
+        elif self.backend == Backends.DYNAMO_DB:
+            raise NotImplemented("read Backends.DYNAMO_DB")
+        elif self.backend == Backends.MONGODB:
+            raise NotImplemented("read Backends.MONGODB")
+        elif self.backend == Backends.MYSQL:
+            raise NotImplemented("read Backends.MYSQL")
+        elif self.backend == Backends.SHELVES:
+            raise NotImplemented("read Backends.SHELVES")
+        elif self.backend == Backends.SQLLIGHT:
+            raise NotImplemented("read Backends.SQLIGHT")
+        elif self.backend == Backends.TEXTFILE:
+            raise NotImplemented("read Backends.TEXTFILE")
+        else:
+            raise ValueError(
+                f"Backend type {self.backend} is not implemented or just wrong")
+
+    def update(self, u_key, u_value):
+        """
+
+        :param u_key: str: the key to search for
+        :param u_value: str: the value to replace the original value with
+        :return: str: the original value, or None if None
+        Does not raise an exception if there is no key/value pair,
+        just returns None.  If the caller
+        has a use case where it needs to raise an exception if trying to
+        update a non-existant key/value pair,
+        then it can call read and let it raise an assertion
+        """
+
+        if self.backend == Backends.DICT:
+            original = self.db_con[u_key]
+            self.db_con[u_key] = u_value
+        elif self.backend == Backends.DYNAMO_DB:
+            raise NotImplemented
+        elif self.backend == Backends.MONGODB:
+            raise NotImplemented
+        elif self.backend == Backends.MYSQL:
+            raise NotImplemented
+        elif self.backend == Backends.SHELVES:
+            raise NotImplemented
+        elif self.backend == Backends.SQLLIGHT:
+            raise NotImplemented
+        elif self.backend == Backends.TEXTFILE:
+            raise NotImplemented
+        else:
+            raise ValueError(
+                f"Backend type {self.backend} is not implemented or just wrong")
+        return original
+
+    def delete(self, d_key):
+        """
+        Delete a key/value pair
+        :rtype: None
+        :param d_key: str: the key to delete
+        :return: None
+        Note: if trying to delete a key/value pair on a key that does not
+        already exist, then just return
+        """
+
+        if self.backend == Backends.DICT:
+            del self.db_con[d_key]
+        elif self.backend == Backends.DYNAMO_DB:
+            raise NotImplemented
+        elif self.backend == Backends.MONGODB:
+            raise NotImplemented
+        elif self.backend == Backends.MYSQL:
+            raise NotImplemented
+        elif self.backend == Backends.SHELVES:
+            raise NotImplemented
+        elif self.backend == Backends.SQLLIGHT:
+            raise NotImplemented
+        elif self.backend == Backends.TEXTFILE:
+            raise NotImplemented
+        else:
+            raise ValueError(
+                f"Backend type {self.backend} is not implemented or just wrong")
+
+    @staticmethod
+    def initialize_sqllite(db_path: str):
+        """
+        :param db_path: str The path to the SQL lite database.  This is a file
+        :return: an sqlite3 connection object
+        """
+        sqlite3 = importlib.import_module(name="sqlite3", package=None)
+        con = sqlite3.connect(db_path)
+        return con
+
+    @staticmethod
+    def initialize_mongodb():
+        """
+
+        :rtype: pymongo.mongo_client.MongoClient    A handle to a Mongo database
+        """
+        pymongo = importlib.import_module(name="pymongo", package="mongo")
+        con = pymongo.MongoClient
+        return con
+
+
+def verify_db(backend_obj: Backend):
+    """
+    :param backend_obj: Backend  Verify the database against the gold standard, 
+    the internal dictionary
+    :return: Number of bad entries
+    """
+    bad_cntr = 0
+    for k in backend_obj.gold.keys():
+        readback = backend_obj.read(k)
+        if readback != backend_obj.gold[k]:
+            print(
+                f"Key {k} is {readback} but should be "
+                f"{backend_obj.gold[k]}. ")
+            bad_cntr += 1
+    return bad_cntr
+
+
+if "__main__" == __name__:
+
+    COUNT = 100  # How many K/V pairs to generate
+    bcknd = Backend(Backends.DICT)
+    for i in range(COUNT):
+        key = uuid.uuid4()
+        value = random_generator(4)
+        bcknd.gold[key] = value
+        bcknd.create(key, value)
+    print("Completed create", file=sys.stderr)
+
+    if verify_db(bcknd) > 0:
+        print(
+            "SOMETHING WENT WRONG DURING THE INITIAL READBACK - PAY "
+            "ATTENTION!!!!!!")
+    print("Completed verify after create test", file=sys.stderr)
+
+    for key in bcknd.gold.keys():
+        value = random_generator(4)
+        bcknd.gold[key] = value
+        bcknd.update(key, value)
+    print("Completed update", file=sys.stderr)
+
+    if verify_db(bcknd) > 0:
+        print("SOMETHING WENT WRONG DURING THE UPDATE - PAY ATTENTION!!!!!!")
+    print("Completed verify after update test", file=sys.stderr)
+
+    for key in bcknd.gold.keys():
+        bcknd.delete(key)
+    print("Completed delete", file=sys.stderr)
+
+    for key in bcknd.gold.keys():
+        try:
+            bcknd.read(key)
+        except KeyError:
+            # We expect a KeyError exception, because the key/value pair
+            # should have been deleted.
+            pass
+        else:
+            print(
+                "SOMETHING WENT WRONG DURING THE DELETE - PAY ATTENTION!!!!!!")
+    print("Completed verify after delete test", file=sys.stderr)
+
