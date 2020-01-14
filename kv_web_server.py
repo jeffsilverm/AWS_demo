fa7f9323707e8bf8d97af6b2f014ded93d1adc5a
Jeffrey Silverman <jsilverman@juniper.net>
2020-01-13 22:02:57 -0800

    Tested with GET, PUT, POST, and DELETE HTTP methods

--- kv_web_server.py
+++ kv_web_server.py
@@ -2,15 +2,19 @@
 #
 # This program implements a web server that uses the Key-Value server
 #
-from flask import Flask
+#
+# To start the web server from a development environment, set these 2 envars
+# $ export FLASK_ENV=development
+# $ export FLASK_APP=kv_web_server.py
+# flask run
 
+from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_worold():
-    return ('Hello, World')
 
-
-
+@app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def hello_world():
+    return f'Hello, World.  Request method is {request.method}'
 
